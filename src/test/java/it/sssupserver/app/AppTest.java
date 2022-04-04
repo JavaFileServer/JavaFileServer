75bc1499352335fd6a8d2525017507d8a6720db5
gioaku <giorgio1099@gmail.com>
2022-04-04 16:25:22 +0200

    first mvn commit

---
+++ src/test/java/it/sssupserver/app/AppTest.java
@@ -0,0 +1,20 @@
+package it.sssupserver.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
