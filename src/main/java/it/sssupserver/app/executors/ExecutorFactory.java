9ab04aa4b2f325991ae4c67a6ec1ceb609a48d27
Tredici <marcolucio1999@gmail.com>
2022-04-15 14:13:21 +0200

    Test first Executor using current FS

--- src/main/java/it/sssupserver/app/executors/ExecutorFactory.java
+++ src/main/java/it/sssupserver/app/executors/ExecutorFactory.java
@@ -3,9 +3,10 @@ package it.sssupserver.app.executors;
 import it.sssupserver.app.executors.samples.*;
 
 public class ExecutorFactory {
-    public static Executor getExecutor()
+    public static Executor getExecutor() throws Exception
     {
         //return new DummyStringExecutor();
-        return new DummyMapExecutor();
+        //return new DummyMapExecutor();
+        return new FlatTmpExecutor();
     }
 }
---
+++ src/main/java/it/sssupserver/app/executors/samples/FlatTmpExecutor.java
@@ -0,0 +1,101 @@
+package it.sssupserver.app.executors.samples;
+
+import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ExecutionException;
+
+import it.sssupserver.app.base.Path;
+import it.sssupserver.app.commands.schedulables.SchedulableCommand;
+import it.sssupserver.app.commands.schedulables.SchedulableCreateOrReplaceCommand;
+import it.sssupserver.app.commands.schedulables.SchedulableReadCommand;
+import it.sssupserver.app.executors.Executor;
+
+public class FlatTmpExecutor implements Executor {
+    private String prefix = "JAVA-FILE-SERVER-";
+    private java.nio.file.Path baseDir;
+
+    static int MAX_CHUNK_SIZE = 2 << 16;
+
+    public FlatTmpExecutor() throws Exception
+    {
+        this.baseDir = Files.createTempDirectory(prefix);
+        System.out.println("Dase directory: " + this.baseDir);
+        this.baseDir.toFile().deleteOnExit();
+    }
+
+    private boolean started;
+    @Override
+    public void start() throws Exception {
+        if (this.started) {
+            throw new Exception("Executor already started");
+        }
+        this.started = true;
+    }
+
+    @Override
+    public void stop() throws Exception {
+        if (!this.started) {
+            throw new Exception("Executor was not previously started started");
+        }
+        this.started = false;
+    }
+
+    private void ensureFlatPath(Path path) throws Exception {
+        if (!path.isFlat()) {
+            throw new Exception("Path is not flat!");
+        }
+    }
+
+    private void handleRead(SchedulableReadCommand command) throws Exception {
+        ensureFlatPath(command.getPath());
+        var filename = command.getPath().toString();
+        var filePath = this.baseDir.resolve(filename);
+        try (var fin = FileChannel.open(filePath, StandardOpenOption.READ)) {
+            var fileSz = fin.size();
+            var toRead = Math.min((int)fileSz, command.getLen() != 0 ? command.getLen() : MAX_CHUNK_SIZE);
+            var bytes = new byte[toRead];
+            var buffer = ByteBuffer.wrap(bytes);
+            fin.read(buffer, command.getBegin());
+            command.reply(bytes);
+        } catch (NoSuchFileException e) {
+            command.notFound();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private void handleCreateOrReplace(SchedulableCreateOrReplaceCommand command) throws Exception {
+        ensureFlatPath(command.getPath());
+        var filename = command.getPath().toString();
+        var filePath = this.baseDir.resolve(filename);
+        try (var fout = FileChannel.open(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE)) {
+            var bytes = command.getData();
+            var buffer = ByteBuffer.wrap(bytes);
+            fout.write(buffer);
+            command.reply(true);
+        } catch (Exception e) {
+            command.reply(false);
+            throw e;
+        }
+    }
+
+    @Override
+    public void execute(SchedulableCommand command) throws Exception {
+        if (command instanceof SchedulableReadCommand) {
+            handleRead((SchedulableReadCommand)command);
+        } else if (command instanceof SchedulableCreateOrReplaceCommand) {
+            handleCreateOrReplace((SchedulableCreateOrReplaceCommand)command);
+        } else {
+            throw new Exception("Unknown command");
+        }
+    }
+
+    @Override
+    public void scheduleExecution(SchedulableCommand command) throws Exception {
+        execute(command);
+    }
+}
