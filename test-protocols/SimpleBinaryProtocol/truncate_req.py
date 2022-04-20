54ae229c34e6a01877a91790f6fc2fb38d1d1fbd
Tredici <marcolucio1999@gmail.com>
2022-04-21 00:33:09 +0200

    Simplify code

--- test-protocols/SimpleBinaryProtocol/truncate_req.py
+++ test-protocols/SimpleBinaryProtocol/truncate_req.py
@@ -20,49 +20,34 @@ def serialize_truncate_message(path):
 def recv_ans(sck):
     print("Receving response from socket")
     # message version
-    v = sck.recv(4, socket.MSG_WAITALL)
-    if len(v) != 4:
-        raise Exception("Bad read "+ str(len(v)))
-    version = int.from_bytes(v, byteorder='big')
-    if version != 1:
-        raise Exception("Bad version, found:", version)
+    check_version(sck, 1)
     # message type
-    t = sck.recv(2, socket.MSG_WAITALL)
-    if len(t) != 2:
-        raise Exception("Bad read")
-    typem = int.from_bytes(t, byteorder='big')
-    if typem != 4:
-        raise Exception("Bad message type, found:", typem)
+    check_type(sck, 4)
     # message category
-    cat = sck.recv(2, socket.MSG_WAITALL)
-    if len(cat) != 2:
-        raise Exception("Bad read")
-    category = int.from_bytes(cat, byteorder='big')
-    if category != 1:
-        raise Exception("Bad category, found:", category)
+    check_category(sck, 1)
     # message status
     s = sck.recv(1, socket.MSG_WAITALL)
     if len(s) != 1:
         raise Exception("Bad read")
     success = int.from_bytes(s, byteorder='big')
     # check padding
-    p = sck.recv(3, socket.MSG_WAITALL)
-    if len(p) != 3:
-        raise Exception("Bad read")
-    padding = int.from_bytes(p, byteorder='big')
-    if padding != 0:
-        raise Exception("Bad padding, found:", padding)
+    check_padding(sck, 3)
     print("File truncated: ", success == 1)
 
 port = 5050
-path = "file/base" if len(sys.argv) == 1 else sys.argv[1]
+
+def usage(comm):
+    print("Usage:")
+    print("\t", comm, "path")
+    exit(1)
 
 if __name__ == "__main__":
-    print("Test WRITE request")
-    s = "Ciao panino al caffééé"
-    b = serialize_string(s)
-    print(s, '=>', b)
-    print('decoded: ', deserialize_string(b))
+    if len(sys.argv) < 2:
+        usage(sys.argv[0])
+
+    path = sys.argv[1]
+
+    print("Test TRUNCATE request")
     cmd = serialize_truncate_message(path)
     print("Write MSG =>", cmd)
     sck = send_cmd(port, cmd)
