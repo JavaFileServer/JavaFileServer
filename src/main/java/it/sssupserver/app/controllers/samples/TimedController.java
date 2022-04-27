fec49c02586445a31ec8f4bcff5d47d7cda62558
Tredici <marcolucio1999@gmail.com>
2022-04-27 22:40:33 +0200

    Add TimedController

---
+++ src/main/java/it/sssupserver/app/controllers/samples/TimedController.java
@@ -0,0 +1,20 @@
+package it.sssupserver.app.controllers.samples;
+
+import it.sssupserver.app.controllers.Controller;
+import it.sssupserver.app.executors.Executor;
+import it.sssupserver.app.handlers.RequestHandler;
+
+public class TimedController implements Controller {
+    private long millis = 5*60*1000;
+
+    public TimedController() {}
+
+    public TimedController(long millis) {
+        this.millis = millis;
+    }
+
+    @Override
+    public void run(Executor executor, RequestHandler requestHandler) throws InterruptedException {
+        Thread.sleep(this.millis);
+    }
+}
