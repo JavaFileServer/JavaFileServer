1b85b613de29858f0d5ead7b1b5552b3e790454b
Tredici <marcolucio1999@gmail.com>
2022-04-27 23:03:50 +0200

    Start controller to controll app termination

--- src/main/java/it/sssupserver/app/App.java
+++ src/main/java/it/sssupserver/app/App.java
@@ -1,6 +1,7 @@
 package it.sssupserver.app;
 
 import it.sssupserver.app.handlers.*;
+import it.sssupserver.app.controllers.ControllerFactory;
 import it.sssupserver.app.executors.*;
 
 /**
@@ -14,12 +15,13 @@ public class App
         System.out.println( "Test handler" );
         var executor = ExecutorFactory.getExecutor();
         var handler = RequestHandlerFactory.getRequestHandler(executor);
+        var controller = ControllerFactory.getController();
         executor.start();
         System.out.println( "Wait for message..." );
         handler.start();
         //var cmd = handler.receiveCommand();
         //System.out.println("Cmd: " + cmd);
-        Thread.sleep(5*60*1000);
+        controller.run(executor, handler);
         handler.stop();
         //handler.receiveAndExecuteCommand();
         executor.stop();
