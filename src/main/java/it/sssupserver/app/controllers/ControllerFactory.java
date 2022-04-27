4d9eeb0c45a4489aadcded6cac15cf56ada3abb5
Tredici <marcolucio1999@gmail.com>
2022-04-27 22:43:52 +0200

    Add ControllerFactory

---
+++ src/main/java/it/sssupserver/app/controllers/ControllerFactory.java
@@ -0,0 +1,12 @@
+package it.sssupserver.app.controllers;
+
+import it.sssupserver.app.controllers.samples.*;
+
+/**
+ * ControllerFactory
+ */
+public class ControllerFactory {
+    public static Controller getController() {
+        return new TimedController();
+    }
+}
