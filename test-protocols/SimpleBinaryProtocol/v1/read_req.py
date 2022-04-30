d42b69ce9c5b99389d8a42b3c936cb72eded742d
Tredici <marcolucio1999@gmail.com>
2022-04-30 14:19:48 +0200

    Clean code

--- test-protocols/SimpleBinaryProtocol/v1/read_req.py
+++ test-protocols/SimpleBinaryProtocol/v1/read_req.py
@@ -52,13 +52,6 @@ def recv_ans(sck):
     elif status == 1:
         # check padding
         check_padding(sck, 2)
-#        p = sck.recv(2, socket.MSG_WAITALL)
-#        if len(p) != 2:
-#            raise Exception("Bad read")
-#        padding = int.from_bytes(p, byteorder='big')
-#        if padding != 0:
-#            raise Exception("Bad flags, found:", padding)
-
         print("Error while reading file")
     else:
         raise Exception("Bad status, found:", status)
@@ -77,12 +70,6 @@ if __name__ == "__main__":
     path = sys.argv[1]
     offset = 0 if len(sys.argv) <= 2 else int(sys.argv[2])
     length = 0 if len(sys.argv) <= 3 else int(sys.argv[3])
-
-
-    #s = "Ciao panino al caffééé"
-    #b = serialize_string(s)
-    #print(s, '=>', b)
-    #print('decoded: ', deserialize_string(b))
     cmd = serialize_write_message(path, offset, length)
     sck = send_cmd(port, cmd)
     sck.settimeout(1)
