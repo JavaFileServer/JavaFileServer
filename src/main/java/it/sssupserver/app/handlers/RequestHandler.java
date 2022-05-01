e0cafd98c954fbfad9438444d546efd18198a073
Tredici <marcolucio1999@gmail.com>
2022-05-01 03:12:48 +0200

    Remove receiveAndExecuteCommand() from RequestHandler interface
    This is done to simplify not used methods while developing new handlers.

--- src/main/java/it/sssupserver/app/handlers/RequestHandler.java
+++ src/main/java/it/sssupserver/app/handlers/RequestHandler.java
@@ -1,7 +1,5 @@
 package it.sssupserver.app.handlers;
 
-import it.sssupserver.app.commands.Command;
-
 /**
  * Interface implemented by all request handlers
  */
@@ -20,18 +18,4 @@ public interface RequestHandler {
      * Blocking.
      */
     public void stop() throws Exception;
-    /**
-     * Wait for a return a command to the caller.
-     * Blocking and synchronous.
-     * To be used mainly for testing purpose.
-     * Does not require the use of start()/stop().
-     */
-    public Command receiveCommand() throws Exception;
-    /**
-     * Wait for a command and execute it.
-     * Blocking and synchronous.
-     * To be used mainly for testing purpose.
-     * Does not require the use of start()/stop().
-     */
-    public void receiveAndExecuteCommand() throws Exception;
 }
