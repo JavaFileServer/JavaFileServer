cc998cdf223cc7ce5e9b4cf3ce9343768a2230f3
Tredici <marcolucio1999@gmail.com>
2022-05-01 02:43:37 +0200

    Improve SchedulableReadCommand
    In a next version support for "reply(byte[] data)" should be dropped in favour of ByteBuffer(s) and scattered IO.

--- src/main/java/it/sssupserver/app/commands/schedulables/SchedulableReadCommand.java
+++ src/main/java/it/sssupserver/app/commands/schedulables/SchedulableReadCommand.java
@@ -1,5 +1,8 @@
 package it.sssupserver.app.commands.schedulables;
 
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 import it.sssupserver.app.commands.ReadCommand;
 
 public abstract class SchedulableReadCommand extends ReadCommand implements SchedulableCommand {
@@ -8,6 +11,17 @@ public abstract class SchedulableReadCommand extends ReadCommand implements Sche
         super(cmd);
     }
 
+    public void reply(FileChannel fc, long offset, long length) throws Exception {
+        var buffer = ByteBuffer.allocate((int)length);
+        fc.write(buffer, offset);
+        this.reply(buffer);
+    }
+    public void reply(ByteBuffer data) throws Exception {
+        var length = data.limit()-data.position();
+        var bytes = new byte[length];
+        data.get(bytes);
+        this.reply(data);
+    }
     public abstract void reply(byte[] data) throws Exception;
     public abstract void notFound() throws Exception;
 }
