ab423facd11f14b11955bd7b431f9feb39ff68e6
Tredici <marcolucio1999@gmail.com>
2022-05-01 18:40:38 +0200

    Start development of cli client

---
+++ clients/client/src/main/java/it/sssupclient/app/App.java
@@ -0,0 +1,13 @@
+package it.sssupclient.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
