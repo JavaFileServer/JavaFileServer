bda89256b20298eb8c4ddfc146e83ecf78b31ee5
Tredici <marcolucio1999@gmail.com>
2022-05-04 16:21:45 +0200

    Test DELETE command

---
+++ clients/client/src/main/java/it/sssupclient/app/command/Delete.java
@@ -0,0 +1,90 @@
+package it.sssupclient.app.command;
+
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+import java.util.Random;
+
+import it.sssupclient.app.BufferManager;
+import it.sssupclient.app.Helpers;
+import it.sssupclient.app.exceptions.InvalidArgumentsException;
+
+public class Delete implements Command {
+    private int version;
+    private String path;
+    private String username = null;
+
+    @Override
+    public void parse(int version, String username, String[] args) throws Exception {
+        this.version = version;
+        this.username = username;
+        if (args.length < 1) {
+            throw new InvalidArgumentsException("Missing required arguments");
+        }
+        this.path = args[0];
+    }
+
+    @Override
+    public void printHelp(String lpadding) {
+        if (lpadding == null) {
+            lpadding = "";
+        }
+        System.err.println(lpadding + getName() + " remote_path");
+        System.err.println(lpadding + "\t test existence of remote file");
+    }
+
+    @Override
+    public String getName() {
+        return "delete";
+    }
+
+    @Override
+    public boolean parseResponseBody(SocketChannel sc) throws Exception {
+        var status = Helpers.readByte(sc);
+        switch (status) {
+        case 0:
+        case 1:
+            Helpers.checkPadding(sc, 3);
+            System.out.println("Success:" + (status == 1));
+            break;
+        default:
+            Helpers.panic("Invalid status: " + status);
+        }
+        return true;
+    }
+
+    @Override
+    public void exec(SocketChannel sc, Scheduler scheduler) throws Exception {
+        this.sendMsg(sc);
+        scheduler.schedule(new Waiter(this.version, getType(), this, this.getMarker()));
+    }
+
+    @Override
+    public short getType() {
+        return 6;
+    }
+
+    public int marker;
+    private void sendMsg(SocketChannel sc) throws IOException {
+        try (var wrapper = BufferManager.getBuffer()) {
+            var buffer = wrapper.get();
+            buffer.putInt(this.version);
+            if (this.version >= 3) {
+                this.marker = new Random().nextInt();
+                buffer.putInt(this.marker);
+            }
+            buffer.putShort(this.getType());
+            buffer.putShort((short)0);
+            if (this.version >= 2) {
+                buffer.put(Helpers.serializeString(this.username));
+            }
+            buffer.put(Helpers.serializeString(this.path));
+            buffer.flip();
+            Helpers.writeAll(sc, buffer);
+        }
+    }
+
+    @Override
+    public int getMarker() {
+        return this.marker;
+    }
+}
