e301a8aebbad1d482c64e9581725b2fa781cbc16
Tredici <marcolucio1999@gmail.com>
2022-05-04 15:29:40 +0200

    Define Append command

---
+++ clients/client/src/main/java/it/sssupclient/app/command/Append.java
@@ -0,0 +1,115 @@
+package it.sssupclient.app.command;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Random;
+
+import it.sssupclient.app.BufferManager;
+import it.sssupclient.app.Helpers;
+import it.sssupclient.app.exceptions.InvalidArgumentsException;
+
+public class Append implements Command {
+    private int version;
+    private String path;
+    private String username = null;
+    private Path src;
+    private FileChannel fin;
+
+    @Override
+    public void parse(int version, String username, String[] args) throws Exception {
+        this.version = version;
+        this.username = username;
+        if (args.length < 2) {
+            throw new InvalidArgumentsException("Missing required arguments");
+        }
+        var file = args[0];
+        var cwd = Paths.get("").toAbsolutePath();
+        var src = cwd.resolve(file);
+        if (!Files.exists(src)) {
+            throw new InvalidArgumentsException("File " + src + " does not exist.");
+        }
+        this.path = args[1];
+        this.src = src;
+        // open file for creation
+        this.fin = FileChannel.open(this.src, StandardOpenOption.READ);
+    }
+
+    @Override
+    public void printHelp(String lpadding) {
+        if (lpadding == null) {
+            lpadding = "";
+        }
+        System.err.println(lpadding + getName() + " local_path remote_path");
+        System.err.println(lpadding + "\t append to the remote path the content of the file at the local_path");
+    }
+
+    @Override
+    public String getName() {
+        return "append";
+    }
+
+    @Override
+    public boolean parseResponseBody(SocketChannel sc) throws Exception {
+        var status = Helpers.readByte(sc);
+        switch (status) {
+        case 0:
+        case 1:
+            Helpers.checkPadding(sc, 3);
+            System.out.println("Success:" + (status == 1));
+            break;
+        default:
+            Helpers.panic("Invalid status: " + status);
+        }
+        return true;
+    }
+
+    @Override
+    public void exec(SocketChannel sc, Scheduler scheduler) throws Exception {
+        this.sendMsg(sc);
+        scheduler.schedule(new Waiter(this.version, getType(), this, this.getMarker()));
+    }
+
+    @Override
+    public short getType() {
+        return 5;
+    }
+
+    public int marker;
+    private void sendMsg(SocketChannel sc) throws IOException {
+        try (var wrapper = BufferManager.getBuffer()) {
+            var buffer = wrapper.get();
+            buffer.putInt(this.version);
+            buffer.putShort(this.getType());
+            buffer.putShort((short)0);
+            if (this.version >= 2) {
+                buffer.put(Helpers.serializeString(this.username));
+            }
+            if (this.version >= 3) {
+                this.marker = new Random().nextInt();
+                buffer.putInt(this.marker);
+            }
+            buffer.put(Helpers.serializeString(this.path));
+            // length
+            var length = this.fin.size();
+            if (this.version < 4) {
+                buffer.putInt((int)length);
+            } else {
+                buffer.putLong(length);
+            }
+            buffer.flip();
+            Helpers.writeAll(sc, buffer);
+            // send all file through socket
+            this.fin.transferTo(0, length, sc);
+        }
+    }
+
+    @Override
+    public int getMarker() {
+        return this.marker;
+    }
+}
