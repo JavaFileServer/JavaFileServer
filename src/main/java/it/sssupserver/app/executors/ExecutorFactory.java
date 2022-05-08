6a73a09980d04a0110c9e893c9ee02bdacb57be0
Tredici <marcolucio1999@gmail.com>
2022-05-09 00:27:40 +0200

    Change braces opening choice

--- src/main/java/it/sssupserver/app/executors/ExecutorFactory.java
+++ src/main/java/it/sssupserver/app/executors/ExecutorFactory.java
@@ -5,8 +5,7 @@ import java.nio.file.Paths;
 import it.sssupserver.app.executors.samples.*;
 
 public class ExecutorFactory {
-    public static Executor getExecutor() throws Exception
-    {
+    public static Executor getExecutor() throws Exception {
         //return new DummyStringExecutor();
         //return new DummyMapExecutor();
         //return new FlatTmpExecutor();
