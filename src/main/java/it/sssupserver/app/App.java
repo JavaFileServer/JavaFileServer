656a5a3e8ea8e3cd65d2717ad7a68dff0b7b8d55
Tredici <marcolucio1999@gmail.com>
2022-05-17 01:19:36 +0200

    Add command line arguments for buffer management

--- src/main/java/it/sssupserver/app/App.java
+++ src/main/java/it/sssupserver/app/App.java
@@ -1,6 +1,7 @@
 package it.sssupserver.app;
 
 import it.sssupserver.app.handlers.*;
+import it.sssupserver.app.base.BufferManager;
 import it.sssupserver.app.controllers.ControllerFactory;
 import it.sssupserver.app.executors.*;
 
@@ -11,11 +12,13 @@ public class App
 {
     static void Help() {
         System.err.println("Usage:");
+        BufferManager.Help("\t");
+        System.out.println();
         ExecutorFactory.Help("\t");
         System.out.println();
         RequestHandlerFactory.Help("\t");
         System.out.println();
-        System.exit(1);
+        System.exit(0);
     }
 
     public static void main( String[] args ) throws Exception
@@ -23,6 +26,7 @@ public class App
         if (args.length > 0 && args[0].equals("--help")) {
             Help();
         }
+        BufferManager.parseArgs(args);
         var executor = ExecutorFactory.getExecutor(args);
         var handler = RequestHandlerFactory.getRequestHandler(executor, args);
         var controller = ControllerFactory.getController();
--- src/main/java/it/sssupserver/app/base/BufferManager.java
+++ src/main/java/it/sssupserver/app/base/BufferManager.java
@@ -2,7 +2,7 @@ package it.sssupserver.app.base;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,10 +11,93 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class BufferManager {
     // no more than MAX_BUFFER_COUNT buffers will be allocated
     private static AtomicInteger buffer_count = new AtomicInteger();
-    public static final int MAX_BUFFER_COUNT = 1 << 9;
+    public static final int MIN_BUFFER_SIZE = 1 << 10;
+    // default number of buffers allocated on demand
+    public static final int DEFAULT_BUFFER_COUNT = 1 << 9;
+    // default size of an allocated buffer
     public static final int DEFAULT_BUFFER_SIZE = 1 << 24;
+    // default upper limit for the memory used by all buffers
+    public static final long DEFAULT_MEMORY_LIMIT = (long)DEFAULT_BUFFER_SIZE*(long)DEFAULT_BUFFER_COUNT;
+    private static int buffer_size = DEFAULT_BUFFER_SIZE;
+    private static int max_buffer_count = DEFAULT_BUFFER_COUNT;
+    
+    // Command line arguments starting with this prefix
+    // are intended as directed to the buffer manager
+    private static final String argsPrefix = "--M";
 
-    private static BlockingQueue<ByteBuffer> bufferQueue = new LinkedBlockingQueue<>(MAX_BUFFER_COUNT);
+    public static void Help(String lpadding) {
+        if (lpadding == null) {
+            lpadding = "";
+        }
+        System.err.println(lpadding + "Arguments recognised by request handlers:");
+        System.err.println(lpadding + "\t" + argsPrefix + "bufsz size: buffer size in KB");
+        System.err.println(lpadding + "\t" + argsPrefix + "limit size: max size in KB for all buffers");
+    }
+
+    public static void parseArgs(String[] args) {
+        try {
+            if (args != null) {
+                int bufsz = 0;
+                long limit = 0L;
+                for (int i=0; i!=args.length; ++i) {
+                    var a = args[i];
+                    if (a.equals("--")) {
+                        break;
+                    } else if (a.startsWith(argsPrefix)) {
+                        var parameter = a.substring(argsPrefix.length());
+                        switch (parameter) {
+                            case "bufsz":
+                                if (i + 1 == args.length) {
+                                    throw new Exception("Missing value for parameter bufsz");
+                                }
+                                bufsz = Integer.parseInt(args[++i]) << 10;
+                                break;
+                            case "limit":
+                                if (i + 1 == args.length) {
+                                    throw new Exception("Missing value for parameter limit");
+                                }
+                                limit = Long.parseLong(args[++i]) << 10;
+                                break;
+                            default:
+                                throw new Exception("Unrecognised argument '" + a + "'");
+                        }
+                    }
+                }
+                if (bufsz == 0) {
+                    bufsz = DEFAULT_BUFFER_SIZE;
+                }
+                if (bufsz < MIN_BUFFER_SIZE) {
+                    throw new Exception("bufsz (" + bufsz + ") cannot be lower than " + MIN_BUFFER_SIZE + " (in bytes)");
+                }
+                if (limit == 0) {
+                    limit = DEFAULT_MEMORY_LIMIT;
+                }
+                if (bufsz < limit) {
+                    throw new Exception("limit (" + limit + ") cannot be lower than bufsz (" + bufsz + ")");
+                }
+                // check args coerence
+                if (limit % bufsz != 0) {
+                    throw new Exception("limit (" + limit + ") is not a multiple of bufsz (" + bufsz + ")");
+                }
+                buffer_size = bufsz;
+                max_buffer_count = (int)(limit / bufsz);
+                // check for overflow
+                if ((long)max_buffer_count != limit / bufsz) {
+                    throw new Exception("Cannot handle " + limit / bufsz + " buffers");
+                }
+                // should queue be resized?
+                if (DEFAULT_BUFFER_COUNT != max_buffer_count) {
+                    bufferQueue = new ArrayBlockingQueue<>(max_buffer_count);
+                }
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            Help("\t");
+            System.exit(1);
+        }
+    }
+
+    private static BlockingQueue<ByteBuffer> bufferQueue = new ArrayBlockingQueue<>(DEFAULT_BUFFER_COUNT);
 
     public static class BufferWrapper implements AutoCloseable {
         private ByteBuffer buffer;
@@ -51,8 +134,8 @@ public class BufferManager {
         } else {
             try {
                 var now = buffer_count.incrementAndGet();
-                if (now <= MAX_BUFFER_COUNT) {
-                    return new BufferWrapper(ByteBuffer.allocateDirect(DEFAULT_BUFFER_SIZE));
+                if (now <= max_buffer_count) {
+                    return new BufferWrapper(ByteBuffer.allocateDirect(buffer_size));
                 } else {
                     return new BufferWrapper(bufferQueue.take());
                 }
