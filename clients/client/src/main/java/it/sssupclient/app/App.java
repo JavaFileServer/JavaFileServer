301dcb0f6bcd193b02aec97bc0cb73a8a1ab6001
Tredici <marcolucio1999@gmail.com>
2022-05-16 18:34:52 +0200

    Add '--help' argument

--- clients/client/src/main/java/it/sssupclient/app/App.java
+++ clients/client/src/main/java/it/sssupclient/app/App.java
@@ -159,6 +159,9 @@ public class App
                         }
                         username = args[++i];
                         break;
+                    case "help":
+                        help();
+                        break;
                     default:
                         panic("Unknown parameter '" + parameter + "'");
                         break;
@@ -173,12 +176,9 @@ public class App
 
     public static void main( String[] args ) throws Exception
     {
-        args = extract_globals(args);
         addCommmands();
+        args = extract_globals(args);
         //addHandlers();
-        if (args.length == 0 || args[0].equals("--help")) {
-            help();
-        }
         handle(args);
     }
 }
