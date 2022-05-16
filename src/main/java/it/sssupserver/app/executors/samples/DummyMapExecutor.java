fe99d592f676cbed3c55f2cfe15612e3f60f9046
Tredici <marcolucio1999@gmail.com>
2022-05-16 14:39:04 +0200

    Remove concept of Replier, problematic for new handlers

--- src/main/java/it/sssupserver/app/executors/samples/DummyMapExecutor.java
+++ src/main/java/it/sssupserver/app/executors/samples/DummyMapExecutor.java
@@ -3,21 +3,13 @@ package it.sssupserver.app.executors.samples;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
-import it.sssupserver.app.executors.ReplyingExecutor;
 import it.sssupserver.app.executors.SynchronousExecutor;
-import it.sssupserver.app.commands.AppendCommand;
-import it.sssupserver.app.commands.Command;
-import it.sssupserver.app.commands.CreateOrReplaceCommand;
-import it.sssupserver.app.commands.ExistsCommand;
-import it.sssupserver.app.commands.ReadCommand;
-import it.sssupserver.app.commands.TruncateCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableAppendCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableCreateOrReplaceCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableExistsCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableReadCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableTruncateCommand;
-import it.sssupserver.app.repliers.Replier;
 
 import java.util.*;
 import java.util.concurrent.*;
@@ -27,29 +19,12 @@ import java.util.function.BiFunction;
  * This dummy executor own a single string
  * that treat as it was the content of a file 
  */
-public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
+public class DummyMapExecutor implements SynchronousExecutor {
     private Map<String,String> content;
 
     public DummyMapExecutor()
     {
         this.content = new ConcurrentHashMap<>();
-    }
-
-    private void handleRead(ReadCommand command, Replier replier) throws Exception
-    {
-        if (command.getBegin() != 0 || command.getLen() != 0)
-        {
-            throw new Exception("Unsupported command options");
-        }
-        var file = command.getPath().toString();
-        var data = this.content.get(file);
-        if (data != null) {
-            var charset = StandardCharsets.UTF_8;
-            var bytes = data.getBytes(charset);
-            replier.replyRead(bytes);
-        } else {
-            replier.replyReadNotFound();
-        }
     }
 
     private void handleRead(SchedulableReadCommand command) throws Exception
@@ -69,17 +44,6 @@ public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
         }
     }
 
-    private void handleCreateOrReplace(CreateOrReplaceCommand command, Replier replier) throws Exception
-    {
-        var file = command.getPath().toString();
-        var data = command.getData();
-        var bytes  = new byte[data.limit()];
-        data.get(bytes);
-        var charset = StandardCharsets.UTF_8;
-        this.content.put(file, new String(bytes, charset));
-        replier.replyCreateOrReplace(true);
-    }
-
     private void handleCreateOrReplace(SchedulableCreateOrReplaceCommand command) throws Exception
     {
         var file = command.getPath().toString();
@@ -89,19 +53,6 @@ public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
         var charset = StandardCharsets.UTF_8;
         this.content.put(file, new String(bytes, charset));
         command.reply(true);
-    }
-
-    private void handleAppend(AppendCommand command, Replier replier) throws Exception
-    {
-        var data = command.getData();
-        var bytes  = new byte[data.limit()];
-        data.get(bytes);
-        var charset = StandardCharsets.UTF_8;
-        var file = command.getPath().toString();
-        var ans = this.content.computeIfPresent(file, (BiFunction<String,String,String>)(String K, String V) -> {
-            return V + new String(bytes, charset);
-        }) != null;
-        replier.replyAppend(ans);
     }
 
     private void handleAppend(SchedulableAppendCommand command) throws Exception
@@ -117,13 +68,6 @@ public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
         command.reply(ans);
     }
 
-    private void handleExists(ExistsCommand command, Replier replier) throws Exception
-    {
-        var file = command.getPath().toString();
-        var ans = this.content.containsKey(file);
-        replier.replyExists(ans);
-    }
-
     private void handleExists(SchedulableExistsCommand command) throws Exception
     {
         var file = command.getPath().toString();
@@ -131,42 +75,11 @@ public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
         command.reply(ans);
     }
 
-    private void handleTruncate(TruncateCommand command, Replier replier) throws Exception
-    {
-        var file = command.getPath().toString();
-        boolean ans = this.content.replace(file, "") != null;
-        replier.replyTruncate(ans);
-    }
-
     private void handleTruncate(SchedulableTruncateCommand command) throws Exception
     {
         var file = command.getPath().toString();
         boolean ans = this.content.replace(file, "") != null;
         command.reply(ans);
-    }
-
-    @Override
-    public void execute(Command command, Replier replier) throws Exception {
-        switch (command.getType())
-        {
-            case READ:
-                handleRead((ReadCommand)command, replier);
-                break;
-            case CREATE_OR_REPLACE:
-                handleCreateOrReplace((CreateOrReplaceCommand)command, replier);
-                break;
-            case EXISTS:
-                handleExists((ExistsCommand)command, replier);
-                break;
-            case TRUNCATE:
-                handleTruncate((TruncateCommand)command, replier);
-                break;
-            case APPEND:
-                handleAppend((AppendCommand)command, replier);
-                break;
-            default:
-                throw new Exception("Unsupported command");
-        }
     }
 
     @Override
@@ -184,12 +97,6 @@ public class DummyMapExecutor implements ReplyingExecutor, SynchronousExecutor {
         } else {
             throw new Exception("Unsupported schedulable command");
         }
-    }
-
-    @Override
-    public void scheduleExecution(Command command, Replier replier) throws Exception
-    {
-        execute(command, replier);
     }
 
     @Override
--- src/main/java/it/sssupserver/app/executors/samples/DummyStringExecutor.java
+++ src/main/java/it/sssupserver/app/executors/samples/DummyStringExecutor.java
@@ -3,7 +3,6 @@ package it.sssupserver.app.executors.samples;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
-import it.sssupserver.app.executors.ReplyingExecutor;
 import it.sssupserver.app.executors.SynchronousExecutor;
 import it.sssupserver.app.commands.AppendCommand;
 import it.sssupserver.app.commands.Command;
@@ -17,13 +16,12 @@ import it.sssupserver.app.commands.schedulables.SchedulableCreateOrReplaceComman
 import it.sssupserver.app.commands.schedulables.SchedulableExistsCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableReadCommand;
 import it.sssupserver.app.commands.schedulables.SchedulableTruncateCommand;
-import it.sssupserver.app.repliers.Replier;
 
 /**
  * This dummy executor own a single string
  * that treat as it was the content of a file 
  */
-public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecutor {
+public class DummyStringExecutor implements SynchronousExecutor {
     private String content;
     public DummyStringExecutor(String data)
     {
@@ -33,17 +31,6 @@ public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecuto
     public DummyStringExecutor()
     {
         this("My super special file content!");
-    }
-
-    private void handleRead(ReadCommand command, Replier replier) throws Exception
-    {
-        if (command.getBegin() != 0 || command.getLen() != 0)
-        {
-            throw new Exception("Unsupported command options");
-        }
-        var charset = StandardCharsets.UTF_8;
-        var bytes = content.getBytes(charset);
-        replier.replyRead(bytes);
     }
 
     private void handleRead(SchedulableReadCommand command) throws Exception
@@ -57,16 +44,6 @@ public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecuto
         command.reply(ByteBuffer.wrap(bytes));
     }
 
-    private void handleCreateOrReplace(CreateOrReplaceCommand command, Replier replier) throws Exception
-    {
-        var data = command.getData();
-        var bytes  = new byte[data.limit()];
-        data.get(bytes);
-        var charset = StandardCharsets.UTF_8;
-        this.content = new String(bytes, charset);
-        replier.replyCreateOrReplace(true);
-    }
-
     private void handleCreateOrReplace(SchedulableCreateOrReplaceCommand command) throws Exception
     {
         var data = command.getData();
@@ -75,16 +52,6 @@ public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecuto
         var charset = StandardCharsets.UTF_8;
         this.content = new String(bytes, charset);
         command.reply(true);
-    }
-
-    private void handleAppend(AppendCommand command, Replier replier) throws Exception
-    {
-        var data = command.getData();
-        var bytes  = new byte[data.limit()];
-        data.get(bytes);
-        var charset = StandardCharsets.UTF_8;
-        this.content += new String(bytes, charset);
-        replier.replyAppend(true);
     }
 
     private void handleAppend(SchedulableAppendCommand command) throws Exception
@@ -96,52 +63,15 @@ public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecuto
         this.content += new String(bytes, charset);
         command.reply(true);
     }
-
-    private void handleExists(ExistsCommand command, Replier replier) throws Exception
-    {
-        // by default file always exists
-        replier.replyExists(true);
-    }
-
     private void handleExists(SchedulableExistsCommand command) throws Exception
     {
         command.reply(true);
-    }
-
-    private void handleTruncate(TruncateCommand command, Replier replier) throws Exception
-    {
-        this.content = "";
-        replier.replyTruncate(true);
     }
 
     private void handleTruncate(SchedulableTruncateCommand command) throws Exception
     {
         this.content = "";
         command.reply(true);
-    }
-
-    @Override
-    public void execute(Command command, Replier replier) throws Exception {
-        switch (command.getType())
-        {
-            case READ:
-                handleRead((ReadCommand)command, replier);
-                break;
-            case CREATE_OR_REPLACE:
-                handleCreateOrReplace((CreateOrReplaceCommand)command, replier);
-                break;
-            case EXISTS:
-                handleExists((ExistsCommand)command, replier);
-                break;
-            case TRUNCATE:
-                handleTruncate((TruncateCommand)command, replier);
-                break;
-            case APPEND:
-                handleAppend((AppendCommand)command, replier);
-                break;
-            default:
-                throw new Exception("Unsupported command");
-        }
     }
 
     @Override
@@ -159,12 +89,6 @@ public class DummyStringExecutor implements ReplyingExecutor, SynchronousExecuto
         } else {
             throw new Exception("Unsupported schedulable command");
         }
-    }
-
-    @Override
-    public void scheduleExecution(Command command, Replier replier) throws Exception
-    {
-        execute(command, replier);
     }
 
     @Override
