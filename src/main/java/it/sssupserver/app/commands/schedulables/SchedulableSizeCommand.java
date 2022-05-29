817f149a240c8f340984386cdd18bad7a65df073
Tredici <marcolucio1999@gmail.com>
2022-05-29 22:17:12 +0200

    Implement submit() method

--- src/main/java/it/sssupserver/app/commands/schedulables/SchedulableSizeCommand.java
+++ src/main/java/it/sssupserver/app/commands/schedulables/SchedulableSizeCommand.java
@@ -1,6 +1,8 @@
 package it.sssupserver.app.commands.schedulables;
 
 import it.sssupserver.app.commands.SizeCommand;
+import it.sssupserver.app.exceptions.ApplicationException;
+import it.sssupserver.app.executors.Executor;
 
 public abstract class SchedulableSizeCommand extends SizeCommand implements SchedulableCommand {
     protected SchedulableSizeCommand(SizeCommand cmd)
@@ -10,4 +12,9 @@ public abstract class SchedulableSizeCommand extends SizeCommand implements Sche
 
     public abstract void reply(long size) throws Exception;
     public abstract void notFound() throws Exception;
+
+    @Override
+    public final void submit(Executor exe) throws ApplicationException {
+        exe.handle(this);
+    }
 }
