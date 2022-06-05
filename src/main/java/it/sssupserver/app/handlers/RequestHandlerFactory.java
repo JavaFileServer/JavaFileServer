e997c48c5b79a1031fc0e3e56ba455ee81b3e193
Tredici <marcolucio1999@gmail.com>
2022-06-05 14:49:48 +0200

    Add --Hhost parameter

--- src/main/java/it/sssupserver/app/handlers/RequestHandlerFactory.java
+++ src/main/java/it/sssupserver/app/handlers/RequestHandlerFactory.java
@@ -14,6 +14,7 @@ public class RequestHandlerFactory {
 
     private static class HandlerArgs {
         public int port = 0;
+        public String host;
     }
 
     public static void Help(String lpadding) {
@@ -22,11 +23,13 @@ public class RequestHandlerFactory {
         }
         System.err.println(lpadding + "Arguments recognised by request handlers:");
         System.err.println(lpadding + "\t" + argsPrefix + "port number: port number on which listen for requests");
+        System.err.println(lpadding + "\t" + argsPrefix + "host hostname/ip: hostname or ip on which listen for requests");
     }
 
     private static HandlerArgs parseArgs(String[] args) {
         var ans = new HandlerArgs();
         try {
+            boolean port = false, host = false;
             if (args != null) for (int i=0; i!=args.length; ++i) {
                 var a = args[i];
                 if (a.equals("--")) {
@@ -35,10 +38,24 @@ public class RequestHandlerFactory {
                     var parameter = a.substring(argsPrefix.length());
                     switch (parameter) {
                         case "port":
+                            if (port) {
+                                throw new Exception("Duplicated parameter port");
+                            }
                             if (i + 1 == args.length) {
                                 throw new Exception("Missing value for parameter port");
                             }
                             ans.port = Integer.parseInt(args[++i]);
+                            port = true;
+                            break;
+                        case "host":
+                            if (host) {
+                                throw new Exception("Duplicated parameter host");
+                            }
+                            if (i + 1 == args.length) {
+                                throw new Exception("Missing value for parameter host");
+                            }
+                            ans.host = args[++i];
+                            host = true;
                             break;
                         default:
                             throw new Exception("Unrecognised argument '" + a + "'");
@@ -59,6 +76,6 @@ public class RequestHandlerFactory {
 
     public static RequestHandler getRequestHandler(FileManager executor, String[] args) throws Exception {
         var a = parseArgs(args);
-        return new SimpleBinaryHandler(executor, a.port);
+        return new SimpleBinaryHandler(executor, a.port, a.host);
     }
 }
